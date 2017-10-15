b31d4eae5b5ad61c675480be4dee29ce6d26067b
lion gates <lion gates>
2017-10-15 22:30:01 +0700

    Add new src

--- WebContent/js/main.js
+++ WebContent/js/main.js
@@ -1,88 +1,48 @@
-jQuery(document).ready(function($){
-    
-    // jQuery sticky Menu
-    
-	$(".mainmenu-area").sticky({topSpacing:0});
-    
-    
-    $('.product-carousel').owlCarousel({
-        loop:true,
-        nav:true,
-        margin:20,
-        responsiveClass:true,
-        responsive:{
-            0:{
-                items:1,
-            },
-            600:{
-                items:3,
-            },
-            1000:{
-                items:5,
-            }
-        }
-    });  
-    
-    $('.related-products-carousel').owlCarousel({
-        loop:true,
-        nav:true,
-        margin:20,
-        responsiveClass:true,
-        responsive:{
-            0:{
-                items:1,
-            },
-            600:{
-                items:2,
-            },
-            1000:{
-                items:2,
-            },
-            1200:{
-                items:3,
-            }
-        }
-    });  
-    
-    $('.brand-list').owlCarousel({
-        loop:true,
-        nav:true,
-        margin:20,
-        responsiveClass:true,
-        responsive:{
-            0:{
-                items:1,
-            },
-            600:{
-                items:3,
-            },
-            1000:{
-                items:4,
-            }
-        }
-    });    
-    
-    
-    // Bootstrap Mobile Menu fix
-    $(".navbar-nav li a").click(function(){
-        $(".navbar-collapse").removeClass('in');
-    });    
-    
-    // jQuery Scroll effect
-    $('.navbar-nav li a, .scroll-to-up').bind('click', function(event) {
-        var $anchor = $(this);
-        var headerH = $('.header-area').outerHeight();
-        $('html, body').stop().animate({
-            scrollTop : $($anchor.attr('href')).offset().top - headerH + "px"
-        }, 1200, 'easeInOutExpo');
+$(document).ready(function() {
+	// Header Scroll
+	$(window).on('scroll', function() {
+		var scroll = $(window).scrollTop();
 
-        event.preventDefault();
-    });    
-    
-    // Bootstrap ScrollPSY
-    $('body').scrollspy({ 
-        target: '.navbar-collapse',
-        offset: 95
-    })      
+		if (scroll >= 50) {
+			$('#header').addClass('fixed');
+		} else {
+			$('#header').removeClass('fixed');
+		}
+	});
+
+	
+	// Page Scroll
+	var sections = $('section')
+		nav = $('nav[role="navigation"]');
+
+	$(window).on('scroll', function () {
+	  	var cur_pos = $(this).scrollTop();
+	  	sections.each(function() {
+	    	var top = $(this).offset().top - 76
+	        	bottom = top + $(this).outerHeight();
+	    	if (cur_pos >= top && cur_pos <= bottom) {
+	      		nav.find('a').removeClass('active');
+	      		nav.find('a[href="#'+$(this).attr('id')+'"]').addClass('active');
+	    	}
+	  	});
+	});
+	nav.find('a').on('click', function () {
+	  	var $el = $(this)
+	    	id = $el.attr('href');
+		$('html, body').animate({
+			scrollTop: $(id).offset().top - 75
+		}, 500);
+	  return false;
+	});
+
+	// Mobile Navigation
+	$('.nav-toggle').on('click', function() {
+		$(this).toggleClass('close-nav');
+		nav.toggleClass('open');
+		return false;
+	});	
+	nav.find('a').on('click', function() {
+		$('.nav-toggle').toggleClass('close-nav');
+		nav.toggleClass('open');
+	});
 });
-
